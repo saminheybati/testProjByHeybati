6fbd451d4be71e2c9189ad06bd05187e4e73d770
samin heybati <zahra.heybati77@gmail.com>
2023-05-19 08:03:57 -0400

    add user list datatable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import {NgxDatatableModule} from "@swimlane/ngx-datatable";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        NgxDatatableModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
